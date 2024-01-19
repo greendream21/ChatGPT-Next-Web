040eec15088e2fc434ab1e72beebcbd988f850af
greendream21 <uri.dev.0802@gmail.com>
2024-01-19 10:35:22 -0500

    # openai key setting feature update

---
+++ app/models/settingModel.ts
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SettingSchema = new Schema(
+  {
+    id: { type: String, required: true },
+    apikey: { type: String },
+    limit: { type: Number },
+  },
+  { timestamps: true },
+);
+const Setting =
+  mongoose.models.tbl_setting || mongoose.model("tbl_setting", SettingSchema);
+
+export default Setting;
