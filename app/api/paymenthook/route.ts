50f95a07f7881ff9bfbf533a2450e21d225c6a7c
greendream21 <uri.dev.0802@gmail.com>
2024-01-22 09:31:34 -0500

    # update

--- app/api/paymenthook/route.ts
+++ app/api/paymenthook/route.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const data = await req.json();
+  const { data } = await req.json();
+
+  const hotmart_token = req.headers.get("x-hotmart-hottok");
 
   console.log("data", data);
-  console.log("Header", req.headers);
+  console.log("Header", hotmart_token);
 
   return NextResponse.json(data);
 }
