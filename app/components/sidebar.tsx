d7938c752a0e1841b3dde87eb7d39de67815c789
greendream21 <uri.dev.0802@gmail.com>
2024-02-26 14:01:21 -0500

    # update the UI

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,8 +121,7 @@ function useDragSideBar() {
   };
 
   const isMobileScreen = useMobileScreen();
-  const shouldNarrow =
-    !isMobileScreen && config.sidebarWidth < MIN_SIDEBAR_WIDTH;
+  const shouldNarrow = false;
 
   useEffect(() => {
     const barWidth = shouldNarrow
@@ -195,7 +194,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          VL GPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
